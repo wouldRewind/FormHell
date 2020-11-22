79a3daa84081ff3d8221d9038fa4d40ce8ca3bca
Suhanov Nick <nik.152002@mail.ru>
2020-11-22 19:58:21 +0300

    maked layout for success page, pre-writted some js func

--- src/js/inputs.js
+++ src/js/inputs.js
@@ -1,6 +1,7 @@
 //"чистит" значение инпутов
 export const clearInputs = inputs => inputs.forEach(inp => inp.value = "")
 // проверяет, все ли инпуты в данной форме заполнены верно
-export const trueValidation = form => form; 
+export const inputIsValid = (inp,type) => typeof inp.value === type;
+
 
 
