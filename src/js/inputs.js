cc3d57e0570f6a98849b24caa4843c2d4da9b13a
Suhanov Nick <nik.152002@mail.ru>
2020-11-22 19:23:53 +0300

    Started from the end

---
+++ src/js/inputs.js
@@ -0,0 +1,6 @@
+//"чистит" значение инпутов
+export const clearInputs = inputs => inputs.forEach(inp => inp.value = "")
+// проверяет, все ли инпуты в данной форме заполнены верно
+export const trueValidation = form => form; 
+
+
