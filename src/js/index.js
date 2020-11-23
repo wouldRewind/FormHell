0956456e7b498bbc1a19b5bcd10fe1df2227297e
Suhanov Nick <nik.152002@mail.ru>
2020-11-23 12:34:25 +0300

    Masked and layouted

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,26 @@
-import {numberInInput} from "./inputs.js";
-
-const numInputs = Array.from(document.querySelectorAll("input[type=number]"));
-
-numInputs.forEach(inp => {
-    inp.addEventListener("focusout",function()
-    {
-        
-        if(numberInInput(inp)) 
+// работа с формами
+$(document).ready(function()
+{
+    $('#phone').mask('+7-000-000-00-00');
+    $('#mail').mask("A", {
+        translation: {
+            "A": { pattern: /[\w@\-.+]/, recursive: true }
+        }
+    });
+    $('input[type=number]').mask('ZZZ', {
+        translation: {
+          'Z': {
+            pattern: /[0-9]/, optional: true
+          }
+        }
     })
+    $('input[type=text]').mask("АААААААААААА", {
+        translation: {
+          'А': {
+            pattern: /^[а-яё]+$/i, optional: true
+          }
+        }
+    })
+        
+
 })
